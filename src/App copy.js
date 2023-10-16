6b5195395345dec16f4aa8153a3a79985527f109
Robert <pinneyr@hotmail.com>
2023-10-16 16:01:40 +0200

    convert to slider and text box

--- src/App copy.js
+++ src/App copy.js
@@ -15,37 +15,21 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
-  function handleReset() {
-    setCount(0);
-    setStep(1);
-  }
-
   return (
     <div>
-      <div>
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={step}
-          onChange={(e) => setStep(Number(e.target.value))}
-        />
+      <div className="step">
+        <button onClick={() => (step > 1 ? setStep((d) => d - 1) : step)}>
+          -
+        </button>
         <span>Step: {step}</span>
+        <button onClick={() => setStep((d) => d + 1)}>+</button>
       </div>
-
-      <div>
+      <div className="count">
         <button onClick={() => setCount((d) => d - step)}>-</button>
-        <span>
-          <input
-            type="text"
-            value={count}
-            onChange={(e) => setCount(Number(e.target.value))}
-          ></input>
-          {console.log(count)}
-        </span>
+        <span>Count: {count}</span>
         <button onClick={() => setCount((d) => d + step)}>+</button>
       </div>
-      <p>
+      <div className="date">
         <span>
           {count === 0
             ? "Today is "
@@ -61,13 +45,7 @@ function Counter() {
         </span>
 
         <span>{date.toDateString()}</span>
-      </p>
-
-      {count !== 0 || step !== 1 ? (
-        <div>
-          <button onClick={handleReset}>Reset</button>
-        </div>
-      ) : null}
+      </div>
     </div>
   );
 }
